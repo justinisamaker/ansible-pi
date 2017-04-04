402ede55396a6584c5bff157a2a9e6b231aa656a
Justin Smith <justin@isamaker.com>
2017-04-03 22:00:45 -0500

    update run command

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #/bin/bash
 
-echo "Do you want to run this playbook on a specific group of hosts? (y/n)"       
+echo "Do you want to run this playbook on a specific group of hosts? (y/n)"
 read specific_hosts
 if [ "$specific_hosts" = "y" ]
 then
   echo "Which hosts group do you want to run this on?"
   read host_group
-  ansible-playbook justin-ansible-pi.yml -i hosts -l $host_group --ask-pass
+  ansible-playbook -s -i hosts ansible-pi.yml -l $host_group
 else
-  ansible-playbook justin-ansible-pi.yml -i hosts --ask-pass
+  ansible-playbook -s -i hosts ansible-pi.yml
 fi
